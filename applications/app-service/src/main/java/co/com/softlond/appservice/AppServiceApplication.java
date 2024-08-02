451c51b88d2c4d83330c156b0c333aff64ca4b74
Daniel Ortiz <andresvertel2021@gmail.com>
2024-08-02 16:40:29 -0500

    Historial terminado

--- applications/app-service/src/main/java/co/com/softlond/appservice/AppServiceApplication.java
+++ applications/app-service/src/main/java/co/com/softlond/appservice/AppServiceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-import reactor.core.publisher.Flux;
+
 import reactor.core.publisher.Mono;
 
 @SpringBootApplication
--- domain/usecase/src/main/java/co/com/softlond/usecase/Plantilla/PlantillaOperationsUseCase.java
+++ domain/usecase/src/main/java/co/com/softlond/usecase/Plantilla/PlantillaOperationsUseCase.java
@@ -1,26 +1,46 @@
 package co.com.softlond.usecase.Plantilla;
 
-import java.sql.Date;
+// import java.sql.Date;
+// import java.util.concurrent.atomic.AtomicInteger;
 
 import org.springframework.stereotype.Service;
 
+import co.com.softlond.model.HistorialModel;
 import co.com.softlond.model.PlantillaModel;
 import co.com.softlond.model.gateways.PlantillaGateways;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 
 @Service
 public class PlantillaOperationsUseCase  {
     
     private final PlantillaGateways plantillaGateways;
+    private final HistorialOperationsUseCase historialOperationsUseCase;
 
-    public PlantillaOperationsUseCase(PlantillaGateways plantillaGateways) {
+    public PlantillaOperationsUseCase(PlantillaGateways plantillaGateways, HistorialOperationsUseCase historialOperationsUseCase) {
         this.plantillaGateways = plantillaGateways;
+        this.historialOperationsUseCase = historialOperationsUseCase;
     }
 
     public Mono<PlantillaModel> savePlantilla(PlantillaModel plantilla) {
-        /* lOGICA DE NEGOCIO */
-        plantilla.setFechaActualizacion(new Date(System.currentTimeMillis()));
-        return plantillaGateways.savePlantilla(plantilla);
+        return plantillaGateways.savePlantilla(plantilla)
+                .doOnSuccess(savedPlantilla -> 
+                saveHistorialAsync(savedPlantilla.getDescripcion())
+                .subscribeOn(Schedulers.boundedElastic())
+                .subscribe());   
     }
 
+    private Mono<Void> saveHistorialAsync(String descripcion){
+        return historialOperationsUseCase.getHistorial()
+                        .defaultIfEmpty(new HistorialModel())
+                        .flatMap(history -> {
+                            history.setContador(null == history.getContador() ? 1 : history.getContador() + 1);
+                            history.setDescripcion(descripcion);
+                            return historialOperationsUseCase.saveHistorial(history);
+                        })
+                        .then();
+    }
+
+    
+    
 }
